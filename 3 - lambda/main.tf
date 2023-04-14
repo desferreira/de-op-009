8515eedf542aed1dbdc91da6b54cabf102a0e7c9
Diego Ferreira <diego.ferreira@paddypowerbetfair.com>
2023-04-14 22:25:31 +0100

    Adiciona correções de permissões da lambda

--- 3 - lambda/main.tf
+++ 3 - lambda/main.tf
@@ -30,13 +30,34 @@ data "aws_iam_policy_document" "assume_role" {
       identifiers = ["lambda.amazonaws.com"]
     }
 
-    actions = ["sts:AssumeRole"]
+    actions = ["sts:AssumeRole"] 
   }
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_para_o_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_policy" "function_logging_policy" {
+  name   = "function-logging-policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
+  role = aws_iam_role.iam_for_lambda.id
+  policy_arn = aws_iam_policy.function_logging_policy.arn
 }
 
 data "archive_file" "lambda" {
@@ -71,6 +92,15 @@ resource "aws_s3_bucket_notification" "aws_lambda_trigger" {
   }
   depends_on = [aws_lambda_permission.test]
 }
+
+resource "aws_cloudwatch_log_group" "function_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.test_lambda.function_name}"
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
 resource "aws_lambda_permission" "test" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
