9df2cefa3b4eb90c03457b61cfce627484452de1
Diego Ferreira <diego.ferreira@paddypowerbetfair.com>
2023-04-14 22:02:09 +0100

    Adiciona arquivos aula 4

--- 3 - lambda/main.tf
+++ 3 - lambda/main.tf
@@ -1,14 +1,24 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  profile    = "ada" # Aqui vai o "profile" que você configurou as credenciais da AWS.
-  region     = "us-east-1" 
+  profile = "ada" # Aqui vai o "profile" que você configurou as credenciais da AWS.
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "de-op-009-bucket-diego-lambda"
+  force_destroy = true
+
+  tags = {
+    Name  = "Meu bucket de testes com lambda"
+    Turma = "DE-OP-009-983"
+  }
 }
 
 data "aws_iam_policy_document" "assume_role" {
@@ -36,10 +46,10 @@ data "archive_file" "lambda" {
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename      = "lambda_function_payload.zip"
   function_name = "minhaLambda"
+  filename      = "lambda_function_payload.zip"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler = "lambda_function.lambda_metodo"
+  handler       = "lambda_function.lambda_metodo"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
@@ -50,4 +60,21 @@ resource "aws_lambda_function" "test_lambda" {
       variavel01 = "valor01"
     }
   }
+}
+
+# Adding S3 bucket as trigger to my lambda and giving the permissions
+resource "aws_s3_bucket_notification" "aws_lambda_trigger" {
+  bucket = aws_s3_bucket.b.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.test_lambda.arn
+    events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+  }
+  depends_on = [aws_lambda_permission.test]
+}
+resource "aws_lambda_permission" "test" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${aws_s3_bucket.b.id}"
 }
