7318b784bb6addbe9b490f3aee9037d504705dda
Diego Ferreira <diego.ferreira@paddypowerbetfair.com>
2023-04-14 10:08:50 +0100

    Adiciona arquivos antes da aula 4

--- 3 - lambda/main.tf
+++ 3 - lambda/main.tf
@@ -10,6 +10,7 @@ provider "aws" {
   profile    = "ada" # Aqui vai o "profile" que você configurou as credenciais da AWS.
   region     = "us-east-1" 
 }
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -35,15 +36,12 @@ data "archive_file" "lambda" {
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
   filename      = "lambda_function_payload.zip"
   function_name = "minhaLambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler = "lambda_function.lambda_metodo"
 
-source_code_hash = data.archive_file.lambda.output_base64sha256
-
+  source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "python3.9"
 
