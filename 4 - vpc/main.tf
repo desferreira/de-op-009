a7e45ec12e769bd18c836b95b61fc2486bfc52c6
Diego Ferreira <diego.ferreira@paddypowerbetfair.com>
2023-04-18 08:41:57 +0100

    Adiciona arquivos pré aula 6

--- 4 - vpc/main.tf
+++ 4 - vpc/main.tf
@@ -1,41 +1,40 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  profile    = "ada"
-  region     = "us-east-1"
+  profile = "ada"
+  region  = "us-east-1"
 }
 
-# 1. Create vpc
 resource "aws_vpc" "dev-vpc" {
-cidr_block = "172.16.1.0/25"
+  cidr_block = "172.16.1.0/25"
 
   tags = {
-    Name = "dev"
+    Name = "VPC 1 - DE-OP-009"
   }
 }
 
 resource "aws_subnet" "private-subnet1" {
   vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block = "172.16.1.48/28"
+  cidr_block        = "172.16.1.48/28"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "private-subnet"
+    Name = "Subnet 1 - DE-OP-009"
   }
 }
 
 resource "aws_subnet" "private-subnet2" {
   vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block = "172.16.1.64/28"
+  cidr_block        = "172.16.1.64/28"
   availability_zone = "us-east-1b"
 
   tags = {
-    Name = "private-subnet2"
+    Name = "Subnet 2 - DE-OP-009"
   }
 }
