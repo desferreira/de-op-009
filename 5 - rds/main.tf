9ff3d5395d5401ecf66fbe2b539c4d6442235122
Diego Ferreira <diego.ferreira@paddypowerbetfair.com>
2023-04-19 08:47:18 +0100

    Adiciona arquivos pré aula 7

--- 5 - rds/main.tf
+++ 5 - rds/main.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  profile    = "ada"
-  region     = "us-east-1"
+  profile = "ada"
+  region  = "us-east-1"
 }
 
 # 1. Create vpc
 resource "aws_vpc" "rds-vpc" {
-cidr_block = "172.16.1.0/25"
+  cidr_block = "172.16.1.0/25"
 
   tags = {
     Name = "dev"
@@ -22,54 +22,51 @@ cidr_block = "172.16.1.0/25"
 
 # Cria uma VPC, um tipo de rede privada dentro da AWS.
 resource "aws_vpc" "dev-vpc" {
-  cidr_block = "172.16.1.0/25"
+  cidr_block = "172.16.1.0/25" # o /25 indica a quantidade de IPs disponíveis para máquinas na rede
 
   tags = {
     Name = "VPC 1 - DE-OP-009"
   }
 }
 
-# Cria uma subnet que pertence àquela rede privada
-resource "aws_subnet" "private-subnet1" {
+# Cria uma subnet que pertence àquela rede privada 
+resource "aws_subnet" "private-subnet" {
+  count             = var.subnet_count
   vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block        = "172.16.1.48/28"
-  availability_zone = "us-east-1a"
+  cidr_block        = var.subnet_cidr_block[count.index] # "172.16.1.0/25" 172.16.1.48 até 172.16.1.64 
+  availability_zone = var.subnet_availability_zone[count.index]
 
   tags = {
-    Name = "Subnet 1 - DE-OP-009"
-  }
-}
-
-# Cria outra subnet que pertence àquela rede privada
-resource "aws_subnet" "private-subnet2" {
-  vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block        = "172.16.1.64/28"
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "Subnet 2 - DE-OP-009"
+    Name = "Subnet ${count.index + 1} - DE-OP-009"
   }
 }
 
 resource "aws_db_subnet_group" "db-subnet" {
-    name = "db_subnet_group"
-    subnet_ids = [aws_subnet.private-subnet1.id, aws_subnet.private-subnet2.id]
+  name       = "db_subnet_group"
+  subnet_ids = [aws_subnet.private-subnet[0].id, aws_subnet.private-subnet[1].id]
 }
-
 
 # Cria um grupo de segurança contendo regras de entrada e saída de rede. 
 # Idealmente, apenas abra o que for necessário e preciso.
 resource "aws_security_group" "allow_db" {
-  name        = "Permite conexão do RDS"
-  description = "Grupo de segurança para permitir conexão ao db"
-  vpc_id      = aws_vpc.rds-vpc.id
+  name        = "permite_conexao_rds"
+  description = "Grupo de seguranca para permitir conexao ao db"
+  vpc_id      = aws_vpc.dev-vpc.id
 
   ingress {
     description = "HTTPS"
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # aws_vpc.dev-vpc.cidr_blocks
+  }
+
+  egress {
+    description = "HTTPS"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # aws_vpc.dev-vpc.cidr_blocks
   }
 
   tags = {
@@ -79,19 +76,26 @@ resource "aws_security_group" "allow_db" {
 
 # Cria uma instância de RDS
 resource "aws_db_instance" "mysql" {
-  allocated_storage    = 10
-  db_name              = "mydb"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  username             = "username"
-  password             = "password"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  db_subnet_group_name = aws_db_subnet_group.db-subnet.name
+  allocated_storage = 10
+  db_name           = "mydb"
+  engine            = "postgres"
+  engine_version    = "12.9"
+  instance_class    = "db.t3.micro"
+  username          = "username" # Nome do usuário "master"
+  password          = "password" # Senha do usuário master
+  port              = 5432
+  # Parâmetro que indica se o DB vai ser acessível publicamente ou não.
+  # Se quiser adicionar isso, preciso de um internet gateway na minha subnet. Em outras palavras, preciso permitir acesso "de fora" da aws.
+  # publicly_accessible    = true
+
+  # Parâmetro que indica se queremos ter um cluster RDS que seja multi az. 
+  # Lembrando, paga-se a mais por isso, mas para ambientes produtivos é essencial.
+  # multi_az               = true
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.db-subnet.name
   vpc_security_group_ids = [aws_security_group.allow_db.id]
 }
 
 output "rds_connection_string" {
   value = aws_db_instance.mysql.endpoint
-} 
+}
